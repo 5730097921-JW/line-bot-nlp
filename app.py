8e8d7e9920bffd3a1bb58ff41a220f3dd10d49ff
5730097921-JW <jessada.wise@gmail.com>
2018-04-30 09:05:17 +0700

    answer

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ with open('intent_arch.json') as json_data:
 intent =  model_from_json(intent_arch)
 intent.load_weights('model_weights.h5')
 dictionary = pickle.load(open('dictionary.pickle','rb'))
+brand_dict_map = {'iphone':'apple','ไอโฟน':'apple','galaxy':'samsung'}
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -98,6 +99,63 @@ def get_intention(sentence):
     # print(intention)
     return intention
 
+def predict_tag(sen, debug=True):
+    current_brand = ''
+    current_model = ''
+    current_color = ''
+    current_capacity = ''
+    current_desc = ''
+    if debug:
+        print('sen:',sen)
+        print('predicted tag:')
+    for brand in brand_dict:
+        if sen.find(brand) != -1:
+            if debug:
+                print('brand = ',brand)
+            current_brand = brand
+            break
+    if current_brand == '':
+        for brand,mapped_brand in brand_dict_map.items():
+            if sen.find(brand) != -1:
+                if debug:
+                    print('brand = ',mapped_brand)
+                current_brand = mapped_brand
+                break
+    if current_brand == '':
+        if debug:
+            print('no brand')
+#         break
+    for model in model_dict:
+        if sen.lower().find(model) != -1:
+            if debug:
+                print('model = ',model)
+            current_model = model
+            break
+    if current_model == '':
+        if debug:
+            print('no model')
+#         break
+    for color in color_dict:
+        if sen.lower().find(color) != -1:
+            if debug:
+                print('color = ',color)
+            current_color = color
+            break
+    if current_color == '':
+        if debug:
+            print('no color')
+#         break
+    for capa in [8,16,32,64,128,256,512]:
+        if re.search(r'{}\s*[Gg][Bb]'.format(capa),sen):
+            current_capacity = capa
+    if current_brand !='' and current_model != '':
+        # not sure should use head or not??
+        current_desc = mobile_df[(mobile_df['brand'] == current_brand) & (mobile_df['model']==current_model)].head(1)
+        if debug:
+            print('desciption:\n',current_desc['description'])
+    
+    return current_brand, current_model, current_color, current_capacity, current_desc
+
 """
 DB (session_id,brand,model,capa,color,price)
 """
@@ -117,18 +175,131 @@ def manage_user(userid,items):
         query = "SELECT * FROM chatbot WHERE `session_id`=%s"
         cursor.execute(sql, (userid))
         result = cursor.fetchone()
+        names = list(items)
+        cols = ', '.join(map(escape_name, names))
         if not result:
-            sql = "INSERT INTO `users` (`sessionid`) VALUES (%s)"
+            sql = "INSERT INTO `chatbot` (`sessionid`) VALUES (%s)"
             cursor.execute(sql, (user_id))
             connection.commit()
-            names = list(items)
-            cols = ', '.join(map(escape_name, names))  # assumes the keys are *valid column names*.
-            placeholders = ', '.join(['%({})s'.format(name) for name in names])
-
-            query = 'INSERT INTO TABLENAME ({}) VALUES ({})'.format(cols, list(items.values()))
+              # assumes the keys are *valid column names*.
+            query = 'INSERT INTO chatbot ({}) VALUES ({})'.format(cols, list(items.values()))
+            cursor.execute(query, items)
+        else:
+            ss = zip(cols, list(items.values())
+            query = 'UPDATE chatbot SET ({}) VALUES ({}) WHERE `session_id`=%s'.format(ss[0],ss[1],userid)
             cursor.execute(query, items)
     return result
 
+intent_dict ={0:'<PRICE>',1:'<INFO>',2:'<BUY>'}
+
+def get_ans(message,intent):
+    tokens = word_tokenize(message)
+    prediction = intent_dict[intent]
+    if prediction == '<PRICE>':
+        if(num_2_label_map[data[i,1]] == '<PRICE>'):
+            hit += 1
+        else:
+            miss += 1
+    elif prediction == '<BUY>':
+        if(num_2_label_map[data[i,1]] == '<BUY>'):
+            hit += 1
+        else:
+            miss += 1
+    elif prediction == '<INFO>':
+        if(num_2_label_map[data[i,1]] == '<INFO>'):
+            hit += 1
+        else:
+            miss += 1
+    else:
+        if(num_2_label_map[data[i,1]] == '<NONE>'):
+            hit += 1
+        else:
+            miss += 1
+    
+    ## tagging part ##
+    current_brand,current_model,current_color,current_capacity,current_desc = predict_tag(data_tag[i,0],debug=debug)
+    for p in all_sen_pairs[i]:
+        if p[1] == 'brand':
+            if not current_brand:
+                tag_miss += 1
+            elif word_tokenize(current_brand)==p[0]:
+                tag_hit += 1
+            else:
+                tag_miss += 1
+        elif p[1] == 'model':
+            if not current_model:
+                tag_miss += 1
+            elif word_tokenize(current_model)==p[0]:
+                tag_hit += 1
+            else:
+                tag_miss += 1
+        elif p[1] == 'color':
+            if not current_color:
+                tag_miss += 1
+            elif word_tokenize(current_color)==p[0]:
+                tag_hit += 1
+            else:
+                tag_miss += 1
+        elif p[1] == 'other':
+            continue
+    answer = ''
+    if current_brand == '':
+        answer = 'กรุณาระบุยี่ห้อด้วยครับ'
+    elif current_model == '':
+        answer = 'กรุณาระบุรุ่นด้วยครับ'
+    elif prediction == '<PRICE>':
+        if current_brand == 'apple':
+            if current_capacity == '':
+                answer = 'กรุณาระบุขนาดความจุด้วยครับ'
+            else:
+                answer = mobile_df[(mobile_df.brand=='apple')&
+                                   (mobile_df.model==current_model)&
+                                   (mobile_df.capacity==current_capacity)]['price']
+        else:
+            answer = mobile_df[(mobile_df.brand==current_brand)&
+                               (mobile_df.model==current_model)]['price']
+    elif prediction == '<INFO>':
+        if current_brand == 'apple':
+            if current_capacity == '':
+                answer = 'กรุณาระบุขนาดความจุด้วยครับ'
+            else:
+                answer = mobile_df[(mobile_df.brand=='apple')&
+                                   (mobile_df.model==current_model)&
+                                   (mobile_df.capacity==current_capacity)]['description']
+        else:
+            answer = mobile_df[(mobile_df.brand==current_brand)&
+                               (mobile_df.model==current_model)]['description']
+    elif prediction == '<BUY>':
+        if current_color == '':
+            answer = 'กรุณาระบุสีที่ต้องการด้วยครับ'
+        elif current_brand == 'apple':
+            if current_capacity == '':
+                answer = 'กรุณาระบุขนาดความจุด้วยครับ'
+            elif current_address == '':
+                answer = 'กรุณาระบุที่อยู่สำหรับรับสินค้าด้วยครับ'
+            else:
+                answer = """กรุณายืนยันการสั่งสินค้าด้วยครับ
+                            brand: {}
+                            model: {}
+                            color: {}
+                            capacity: {}
+                            address: {}""".format(current_brand,
+                                                  current_model,
+                                                  current_color,
+                                                  current_capacity,
+                                                  current_address)
+        elif current_address == '':
+            answer = 'กรุณาระบุที่อยู่สำหรับรับสินค้าด้วยครับ'
+        else:
+            answer = """กรุณายืนยันการสั่งสินค้าด้วยครับ
+                        brand: {}
+                        model: {}
+                        color: {}
+                        address: {}""".format(current_brand,
+                                              current_model,
+                                              current_color,
+                                              current_address)
+    return answer
 
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
@@ -136,9 +307,10 @@ def message_text(event):
     userid = event.source.user_id
     # print(message)
     intention = get_intention(message)
+    ans=get_ans(message,intention)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=(message+str(intention)))
+        TextSendMessage(text=(ans))
     )
 
 
